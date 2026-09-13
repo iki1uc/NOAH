e39ac35f5d752e0e92cee9acddca23b3b838a43b
iki1uc <entertrainer@web.de>
2026-09-13 21:08:15 +0200

    Create window.RUNTIME.js

---
+++ window.RUNTIME.js
@@ -0,0 +1,12 @@
+window.RUNTIME = {
+    move: 0,
+    sein: "ruhe",
+    mainTick: 0,
+    atemPhase: 0,
+    atom: 50,
+    o2: 50,
+    co2: 50,
+    h2o: 50,
+    wonder: 0,
+    djinn: false
+};
