ac060ae7cd143160594e874d558e0df360dcd46d
iki1uc <entertrainer@web.de>
2026-09-13 21:09:55 +0200

    Create Bridge.js to initialize BRIDGE object

---
+++ Bridge.js
@@ -0,0 +1,13 @@
+import "./NOAH.js";
+import "./SLI.js";
+import "./brücke.js";
+import "./continuum-bridge.js";
+import "./vector.energie.js";
+
+window.BRIDGE = {
+    noah: NOAH,
+    sli: SLI,
+    bruecke: BRUECKE,
+    continuum: CONTINUUM,
+    energie: ENERGIE
+};
