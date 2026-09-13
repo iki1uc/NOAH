d63669cc80b92a82e509c5bccc8db2e3aab61d4e
iki1uc <entertrainer@web.de>
2026-09-13 21:09:05 +0200

    Create UI‑Engine.js

---
+++ UI‑Engine.js
@@ -0,0 +1,5 @@
+function updateUI(){
+    document.getElementById("mainTick").innerText = RUNTIME.mainTick;
+    document.getElementById("sein").innerText = RUNTIME.sein;
+    document.getElementById("move").innerText = RUNTIME.move;
+}
